cdd2dfa4757fb326670e3aaefa8d0a37b2632005
shakilahmed0369 <sakilhossain01969@gmail.com>
2023-10-02 17:41:15 +0600

    Multi Auth - Adding Default Seeders For User and Admin

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::insert([
+            [
+                'name' => 'Super Admin',
+                'email' => 'admin@gmail.com',
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+                'user_type' => 'admin'
+            ],
+            [
+                'name' => 'Jhone deo',
+                'email' => 'user@gmail.com',
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+                'user_type' => 'user'
+            ]
+        ]);
+    }
+}
