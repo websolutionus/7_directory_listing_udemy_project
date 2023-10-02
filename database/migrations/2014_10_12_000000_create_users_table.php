f188b29dea26eea6a90325f6c78d423d3f3521bf
shakilahmed0369 <sakilhossain01969@gmail.com>
2023-10-02 16:31:45 +0600

    Multi Auth - Adding New Columns in User Table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('avatar')->default('/default/avatar.png');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('user_type', ['user', 'admin'])->default('user');
             $table->rememberToken();
             $table->timestamps();
         });
