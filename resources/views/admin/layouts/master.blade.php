11fdc856a6d05f68717aa38df9eb3731f76e239e
shakilahmed0369 <sakilhossain01969@gmail.com>
2023-10-04 10:29:06 +0600

    Admin Setup - Mastering Admin Login Template (Part - 1)

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -10,9 +10,6 @@
   <link rel="stylesheet" href="{{ asset('admin/assets/modules/fontawesome/css/all.min.css') }}">
 
   <!-- CSS Libraries -->
-  <link rel="stylesheet" href="{{ asset('admin/assets/modules/jqvmap/dist/jqvmap.min.css') }}">
-  <link rel="stylesheet" href="{{ asset('admin/assets/modules/weather-icon/css/weather-icons.min.css') }}">
-  <link rel="stylesheet" href="{{ asset('admin/assets/modules/weather-icon/css/weather-icons-wind.min.css') }}">
   <link rel="stylesheet" href="{{ asset('admin/assets/modules/summernote/summernote-bs4.css') }}">
 
   <!-- Template CSS -->
@@ -45,25 +42,14 @@
   <!-- General JS Scripts -->
   <script src="{{ asset('admin/assets/modules/jquery.min.js') }}"></script>
   <script src="{{ asset('admin/assets/modules/popper.js') }}"></script>
-  <script src="{{ asset('admin/assets/modules/tooltip.js') }}"></script>
   <script src="{{ asset('admin/assets/modules/bootstrap/js/bootstrap.min.js') }}"></script>
   <script src="{{ asset('admin/assets/modules/nicescroll/jquery.nicescroll.min.js') }}"></script>
-  <script src="{{ asset('admin/assets/modules/moment.min.js') }}"></script>
   <script src="{{ asset('admin/assets/js/stisla.js') }}"></script>
 
   <!-- JS Libraies -->
-  <script src="{{ asset('admin/assets/modules/simple-weather/jquery.simpleWeather.min.js') }}"></script>
-  <script src="{{ asset('admin/assets/modules/chart.min.js') }}"></script>
-  <script src="{{ asset('admin/assets/modules/jqvmap/dist/jquery.vmap.min.js') }}"></script>
-  <script src="{{ asset('admin/assets/modules/jqvmap/dist/maps/jquery.vmap.world.js') }}"></script>
   <script src="{{ asset('admin/assets/modules/summernote/summernote-bs4.js') }}"></script>
-  <script src="{{ asset('admin/assets/modules/chocolat/dist/js/jquery.chocolat.min.js') }}"></script>
-
-  <!-- Page Specific JS File -->
-  <script src="{{ asset('admin/assets/js/page/index-0.js') }}"></script>
 
   <!-- Template JS File -->
   <script src="{{ asset('admin/assets/js/scripts.js') }}"></script>
-  <script src="{{ asset('admin/assets/js/custom.js') }}"></script>
 </body>
 </html>
