c0d631d3770714ecdebf146e0dd181f41116be18
Shakil Ahmed <sakilhossain01969@gmail.com>
2023-10-11 11:59:35 +0600

    Listing Category - Working with Update Feature

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -5,9 +5,11 @@ namespace App\Http\Controllers\Admin;
 use App\DataTables\CategoryDataTable;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\CategoryStoreRequest;
+use App\Http\Requests\Admin\CategoryUpdateRequest;
 use App\Models\Category;
 use App\Traits\FileUploadTrait;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 use Str;
@@ -34,7 +36,7 @@ class CategoryController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(CategoryStoreRequest $request)
+    public function store(CategoryStoreRequest $request) : RedirectResponse
     {
         $iconPath = $this->uploadImage($request, 'image_icon');
         $backgroundPath = $this->uploadImage($request, 'background_image');
@@ -54,27 +56,34 @@ class CategoryController extends Controller
     }
 
     /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(string $id) : View
     {
-        //
+        $category = Category::findOrFail($id);
+        return view('admin.category.edit', compact('category'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(CategoryUpdateRequest $request, string $id) : RedirectResponse
     {
-        //
+        $iconPath = $this->uploadImage($request, 'image_icon', $request->old_icon);
+        $backgroundPath = $this->uploadImage($request, 'background_image', $request->olb_background);
+
+        $category = Category::findOrFail($id);
+        $category->image_icon = !empty($iconPath) ? $iconPath : $request->old_icon;
+        $category->background_image = !empty($backgroundPath) ? $backgroundPath : $request->old_background;
+        $category->name = $request->name;
+        $category->slug = Str::slug($request->name);
+        $category->show_at_home = $request->show_at_home;
+        $category->status = $request->status;
+        $category->save();
+
+        toastr()->success('Update Successfully!');
+
+        return to_route('admin.category.index');
     }
 
     /**
