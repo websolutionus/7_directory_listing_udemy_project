a2405cf6398edbc54e6c3a451fbe4dcca358792b
Shakil Ahmed <sakilhossain01969@gmail.com>
2023-10-11 17:13:47 +0600

    Listing Locations - Creating Necessary Files and Designs

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -193,6 +193,8 @@
                         class="fas fa-columns"></i> <span>Listings</span></a>
                 <ul class="dropdown-menu">
                     <li><a class="nav-link" href="{{ route('admin.category.index') }}">Categories</a></li>
+                    <li><a class="nav-link" href="{{ route('admin.location.index') }}">Lcoation</a></li>
+
 
                 </ul>
             </li>
--- routes/admin.php
+++ routes/admin.php
@@ -3,6 +3,7 @@ use App\Http\Controllers\Admin\AdminAuthController;
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\HeroController;
+use App\Http\Controllers\Admin\LocationController;
 use App\Http\Controllers\Admin\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -30,6 +31,9 @@ Route::group([
 
     /** Category Routes */
     Route::resource('/category', CategoryController::class);
+
+    /** Category Routes */
+    Route::resource('/location', LocationController::class);
 
 
 });
