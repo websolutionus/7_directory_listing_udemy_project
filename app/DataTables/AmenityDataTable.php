c3a5776cb02559797342f794b84096a0ca828929
Shakil Ahmed <sakilhossain01969@gmail.com>
2023-10-12 15:48:43 +0600

    Amenities - Showing Data in Datatable

--- app/DataTables/AmenityDataTable.php
+++ app/DataTables/AmenityDataTable.php
@@ -22,7 +22,23 @@ class AmenityDataTable extends DataTable
     public function dataTable(QueryBuilder $query): EloquentDataTable
     {
         return (new EloquentDataTable($query))
-            ->addColumn('action', 'amenity.action')
+            ->addColumn('action', function($query){
+                $edit = '<a href="'.route('admin.amenity.edit', $query->id).'" class="btn btn-sm btn-primary"><i class="fas fa-edit"></i></a>';
+                $delete = '<a href="'.route('admin.amenity.destroy', $query->id).'" class="delete-item btn btn-sm btn-danger ml-2"><i class="fas fa-trash"></i></a>';
+
+                return $edit.$delete;
+            })
+            ->addColumn('icon', function($query){
+                return '<i class="'.$query->icon.'" style="font-size:40px"></i>';
+            })
+            ->addColumn('status', function($query){
+                if($query->status === 1){
+                    return "<span class='badge badge-primary'>Active</span>";
+                }else{
+                    return "<span class='badge badge-danger'>Inactive</span>";
+                }
+            })
+            ->rawColumns(['icon', 'action', 'status'])
             ->setRowId('id');
     }
 
@@ -62,15 +78,17 @@ class AmenityDataTable extends DataTable
     public function getColumns(): array
     {
         return [
-            Column::computed('action')
-                  ->exportable(false)
-                  ->printable(false)
-                  ->width(60)
-                  ->addClass('text-center'),
+
             Column::make('id'),
-            Column::make('add your columns'),
-            Column::make('created_at'),
-            Column::make('updated_at'),
+            Column::make('icon'),
+            Column::make('name'),
+            Column::make('slug'),
+            Column::make('status'),
+            Column::computed('action')
+            ->exportable(false)
+            ->printable(false)
+            ->width(150)
+            ->addClass('text-center'),
         ];
     }
 
