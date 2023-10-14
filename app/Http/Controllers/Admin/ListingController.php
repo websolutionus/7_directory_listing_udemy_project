44eb710b85b27c61d7661711efefd4cae6f5c5ff
Shakil Ahmed <sakilhossain01969@gmail.com>
2023-10-14 15:55:03 +0600

    Listing Feature - Working with Create Feature (Part - 3)

--- app/Http/Controllers/Admin/ListingController.php
+++ app/Http/Controllers/Admin/ListingController.php
@@ -37,7 +37,7 @@ class ListingController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        dd($request->all());
     }
 
     /**
--- resources/views/admin/listing/create.blade.php
+++ resources/views/admin/listing/create.blade.php
@@ -31,7 +31,7 @@
                                             <label for="">Image <span class="text-danger">*</span></label>
                                             <div id="image-preview" class="image-preview">
                                                 <label for="image-upload" id="image-label">Choose File</label>
-                                                <input type="file" name="image" id="image-upload" />
+                                                <input type="file" name="image" id="image-upload" required />
                                             </div>
                                         </div>
                                     </div>
@@ -40,7 +40,7 @@
                                             <label for="">Thumbnail Image <span class="text-danger">*</span></label>
                                             <div id="image-preview-2" class="image-preview">
                                                 <label for="image-upload-2" id="image-label-2">Choose File</label>
-                                                <input type="file" name="thumbnail_image" id="image-upload-2" />
+                                                <input type="file" name="thumbnail_image" id="image-upload-2" required />
                                             </div>
                                         </div>
                                     </div>
@@ -48,13 +48,13 @@
 
                                 <div class="form-group">
                                     <label for="">Title <span class="text-danger">*</span></label>
-                                    <input type="text" class="form-control" name="title">
+                                    <input type="text" class="form-control" name="title" required>
                                 </div>
                                 <div class="row">
                                     <div class="col-md-6">
                                         <div class="form-group">
                                             <label for="">category <span class="text-danger">*</span></label>
-                                            <select name="status" class="form-control">
+                                            <select name="status" class="form-control" name="category" required>
                                                 <option value="">Select</option>
                                                 @foreach ($categories as $category)
                                                 <option value="{{ $category->id }}">{{ $category->name }}</option>
@@ -65,7 +65,7 @@
                                     <div class="col-md-6">
                                         <div class="form-group">
                                             <label for="">Location <span class="text-danger">*</span></label>
-                                            <select name="status" class="form-control">
+                                            <select name="status" class="form-control" name="location" required>
                                                 <option value="">Select</option>
                                                 @foreach ($locations as $location)
                                                 <option value="{{ $location->id }}">{{ $location->name }}</option>
@@ -77,64 +77,64 @@
 
                                 <div class="form-group">
                                     <label for="">Address <span class="text-danger">*</span></label>
-                                    <input type="text" class="form-control" name="title">
+                                    <input type="text" class="form-control" name="address" required>
                                 </div>
 
                                 <div class="row">
                                     <div class="col-md-6">
                                         <div class="form-group">
                                             <label for="">Phone <span class="text-danger">*</span></label>
-                                            <input type="text" class="form-control" name="title">
+                                            <input type="text" class="form-control" name="phone" required>
                                         </div>
                                     </div>
                                     <div class="col-md-6">
                                         <div class="form-group">
                                             <label for="">Email <span class="text-danger">*</span></label>
-                                            <input type="text" class="form-control" name="title">
+                                            <input type="text" class="form-control" name="email" required>
                                         </div>
                                     </div>
 
                                     <div class="col-md-12">
                                         <div class="form-group">
                                             <label for="">Website <span class="text-danger"></span></label>
-                                            <input type="text" class="form-control" name="title">
+                                            <input type="text" class="form-control" name="website">
                                         </div>
                                     </div>
                                     <div class="col-md-3">
                                         <div class="form-group">
                                             <label for="">Facebook Link <span class="text-danger"></span></label>
-                                            <input type="text" class="form-control" name="title">
+                                            <input type="text" class="form-control" name="facebook_link">
                                         </div>
                                     </div>
                                     <div class="col-md-3">
                                         <div class="form-group">
                                             <label for="">X Link <span class="text-danger"></span></label>
-                                            <input type="text" class="form-control" name="title">
+                                            <input type="text" class="form-control" name="x_link">
                                         </div>
                                     </div>
                                     <div class="col-md-3">
                                         <div class="form-group">
                                             <label for="">Linkedin Link <span class="text-danger"></span></label>
-                                            <input type="text" class="form-control" name="title">
+                                            <input type="text" class="form-control" name="linkedin_link">
                                         </div>
                                     </div>
                                     <div class="col-md-3">
                                         <div class="form-group">
                                             <label for="">Whatsapp Link <span class="text-danger"></span></label>
-                                            <input type="text" class="form-control" name="title">
+                                            <input type="text" class="form-control" name="whatsapp_link">
                                         </div>
                                     </div>
                                     <div class="col-md-12">
                                         <div class="form-group">
                                             <label for="">Attacement <span class="text-danger"></span></label>
-                                            <input type="file" class="form-control" name="title">
+                                            <input type="file" class="form-control" name="attachment">
                                         </div>
                                     </div>
                                 </div>
 
                                 <div class="form-group">
                                     <label>Amenities</label>
-                                    <select class="form-control select2" multiple="">
+                                    <select class="form-control select2" multiple="" name="amenities[]">
                                         @foreach ($amenities as $amenity)
                                         <option value="{{ $amenity->id }}" >{{ $amenity->name }}</option>
                                         @endforeach
@@ -142,30 +142,30 @@
                                 </div>
 
                                 <div class="form-group">
-                                    <label for="">Description <span class="text-danger"></span></label>
-                                    <textarea name="" class="summernote"></textarea>
+                                    <label for="">Description <span class="text-danger">*</span></label>
+                                    <textarea name="description" class="summernote" required ></textarea>
                                 </div>
 
                                 <div class="form-group">
                                     <label for="">Google Map Embed Code <span class="text-danger"></span></label>
-                                    <textarea name="" class="form-control"></textarea>
+                                    <textarea name="google_map_embed_code" class="form-control"></textarea>
                                 </div>
 
                                 <div class="form-group">
                                     <label for="">Seo Title <span class="text-danger"></span></label>
-                                    <input type="text" class="form-control" name="title">
+                                    <input type="text" class="form-control" name="seo_title">
                                 </div>
 
                                 <div class="form-group">
                                     <label for="">Seo Description <span class="text-danger"></span></label>
-                                    <textarea name="" class="form-control"></textarea>
+                                    <textarea name="seo_description" class="form-control"></textarea>
                                 </div>
 
                                 <div class="row">
                                     <div class="col-md-4">
                                         <div class="form-group">
                                             <label for="">Status <span class="text-danger">*</span></label>
-                                            <select name="status" class="form-control">
+                                            <select name="status" class="form-control" required>
                                                 <option value="1">Active</option>
                                                 <option value="0">Inactive</option>
                                             </select>
@@ -174,7 +174,7 @@
                                     <div class="col-md-4">
                                         <div class="form-group">
                                             <label for="">Is Featured <span class="text-danger"></span></label>
-                                            <select name="status" class="form-control">
+                                            <select name="is_featured" class="form-control" required>
                                                 <option value="0">No</option>
                                                 <option value="1">Yes</option>
                                             </select>
@@ -183,7 +183,7 @@
                                     <div class="col-md-4">
                                         <div class="form-group">
                                             <label for="">Is Verified <span class="text-danger"></span></label>
-                                            <select name="status" class="form-control">
+                                            <select name="is_verified" class="form-control" required>
                                                 <option value="0">No</option>
                                                 <option value="1">Yes</option>
                                             </select>
