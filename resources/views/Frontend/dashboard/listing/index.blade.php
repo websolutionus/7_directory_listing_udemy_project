4938d5085bc080a7ece1a901fdab35d6e33b0509
Shakil Ahmed <sakilhossain01969@gmail.com>
2023-10-20 09:25:08 +0600

    Listing - Working with Listing Delete Feature

--- resources/views/Frontend/dashboard/listing/index.blade.php
+++ resources/views/Frontend/dashboard/listing/index.blade.php
@@ -25,4 +25,53 @@
 
 @push('scripts')
     {{ $dataTable->scripts(attributes: ['type' => 'module']) }}
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    
+    <script>
+        $('body').on('click', '.delete-item', function(e) {
+            e.preventDefault();
+            let url = $(this).attr('href');
+
+            Swal.fire({
+                title: 'Are you sure?',
+                text: "You won't be able to revert this!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Yes, delete it!'
+            }).then((result) => {
+                if (result.isConfirmed) {
+
+                    $.ajax({
+                        method: 'DELETE',
+                        url: url,
+                        data: {_token: "{{ csrf_token() }}"},
+                        success: function(response) {
+                            if(response.status === 'success'){
+                                Swal.fire(
+                                    'Deleted!',
+                                    response.message,
+                                    'success'
+                                )
+                                window.location.reload();
+                            }else if (response.status === 'error'){
+                                Swal.fire(
+                                    'Somthing wen\'t wrong!',
+                                    response.message,
+                                    'error'
+                                )
+                            }
+                        },
+                        error: function(xhr, status, error){
+                            console.log(error);
+                        }
+
+                    })
+
+                }
+            })
+
+        })
+    </script>
 @endpush
