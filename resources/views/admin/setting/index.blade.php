9e4949e658437f083743dfb70cfbf29c6f932e67
Shakil Ahmed <sakilhossain01969@gmail.com>
2023-10-22 10:41:25 +0600

    General Settings -  Working with Setting Update Feature (Part - 1)

--- resources/views/admin/setting/index.blade.php
+++ resources/views/admin/setting/index.blade.php
@@ -18,14 +18,14 @@
                 <div class="col-12">
                     <div class="card">
                         <div class="card-header">
-                          <h4>2 Column</h4>
+                          <h4>Settings</h4>
                         </div>
                         <div class="card-body">
                           <div class="row">
                             <div class="col-12 col-sm-12 col-md-2">
                               <ul class="nav nav-pills flex-column" id="myTab4" role="tablist">
                                 <li class="nav-item">
-                                  <a class="nav-link active" id="home-tab4" data-toggle="tab" href="#home4" role="tab" aria-controls="home" aria-selected="true">Home</a>
+                                  <a class="nav-link active" id="home-tab4" data-toggle="tab" href="#home4" role="tab" aria-controls="home" aria-selected="true">General Settings</a>
                                 </li>
                                 <li class="nav-item">
                                   <a class="nav-link" id="profile-tab4" data-toggle="tab" href="#profile4" role="tab" aria-controls="profile" aria-selected="false">Profile</a>
@@ -38,12 +38,62 @@
                             <div class="col-12 col-sm-12 col-md-10">
                               <div class="tab-content no-padding" id="myTab2Content">
                                 <div class="tab-pane fade show active" id="home4" role="tabpanel" aria-labelledby="home-tab4">
-                                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                                  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                                  consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                                  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                                  proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                                  <div class="card border">
+                                    <div class="card-body">
+                                        <form action="">
+                                            <div class="row">
+                                                <div class="col-md-12">
+                                                    <div class="form-group">
+                                                        <label for="">Site Name</label>
+                                                        <input type="text" class="form-control" name="site_name">
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <label for="">Site Email</label>
+                                                        <input type="text" class="form-control" name="site_email">
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <label for="">Site Phone</label>
+                                                        <input type="text" class="form-control" name="site_phone">
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-12">
+                                                    <div class="form-group">
+                                                        <label for="">Site Default Currency</label>
+                                                        <Select name="site_default_currency" class="form-control select2">
+                                                            <option value="">Select</option>
+                                                            @foreach (config('currencies.currency_list') as $key => $currency)
+                                                                <option value="{{ $currency }}">{{ $key }} ({{ $currency }})</option>
+                                                            @endforeach
+
+                                                        </Select>
+                                                    </div>
+                                                </div>
+
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <label for="">Site Currency Icon</label>
+                                                        <input type="text" class="form-control" name="site_currency_icon">
+                                                    </div>
+                                                </div>
+
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <label for="">Site Currency Possition</label>
+                                                        <Select name="site_currency_postion" class="form-control">
+                                                            <option value="left">Left</option>
+                                                            <option value="right">Right</option>
+                                                        </Select>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <button type="submit" class="btn btn-primary">Save</button>
+                                        </form>
+                                    </div>
+                                  </div>
                                 </div>
                                 <div class="tab-pane fade" id="profile4" role="tabpanel" aria-labelledby="profile-tab4">
                                   Sed sed metus vel lacus hendrerit tempus. Sed efficitur velit tortor, ac efficitur est lobortis quis. Nullam lacinia metus erat, sed fermentum justo rutrum ultrices. Proin quis iaculis tellus. Etiam ac vehicula eros, pharetra consectetur dui. Aliquam convallis neque eget tellus efficitur, eget maximus massa imperdiet. Morbi a mattis velit. Donec hendrerit venenatis justo, eget scelerisque tellus pharetra a.
