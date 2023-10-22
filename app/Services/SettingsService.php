607b693a8290ec44282691b5c88a528c1615a8c5
Shakil Ahmed <sakilhossain01969@gmail.com>
2023-10-22 12:51:14 +0600

    General Settings -  Making Settings Data Globally Available ( Part -1 )

---
+++ app/Services/SettingsService.php
@@ -0,0 +1,23 @@
+<?php
+namespace App\Services;
+
+use App\Models\Setting;
+use Cache;
+
+class SettingsService {
+
+    function getSettings() {
+        return Cache::rememberForever('settings', function(){
+            return Setting::pluck('value', 'key')->toArray();
+        });
+    }
+
+    function setGlobalSettings() {
+        $settings = $this->getSettings();
+        config()->set('settings', $settings);
+    }
+
+    function clearCachedSettings() {
+        Cache::forget('settings');
+    }
+}
