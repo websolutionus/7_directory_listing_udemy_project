21004b029679ace18d69b5937fb56c780cbc2965
Shakil Ahmed <sakilhossain01969@gmail.com>
2023-10-23 10:13:39 +0600

    PayPal Setting- Working with Paypal Settings Form ( Part - 1)

--- resources/views/admin/payment-setting/index.blade.php
+++ resources/views/admin/payment-setting/index.blade.php
@@ -37,65 +37,9 @@
                             </div>
                             <div class="col-12 col-sm-12 col-md-10">
                               <div class="tab-content no-padding" id="myTab2Content">
-                                <div class="tab-pane fade show active" id="home4" role="tabpanel" aria-labelledby="home-tab4">
-                                  <div class="card border">
-                                    <div class="card-body">
-                                        <form action="{{ route("admin.general-settings.update") }}" method="POST">
-                                            @csrf
-                                            <div class="row">
-                                                <div class="col-md-12">
-                                                    <div class="form-group">
-                                                        <label for="">Site Name</label>
-                                                        <input type="text" class="form-control" name="site_name" value="{{ config('settings.site_name') }}">
-                                                    </div>
-                                                </div>
-                                                <div class="col-md-6">
-                                                    <div class="form-group">
-                                                        <label for="">Site Email</label>
-                                                        <input type="text" class="form-control" name="site_email" value="{{ config('settings.site_email') }}">
-                                                    </div>
-                                                </div>
-                                                <div class="col-md-6">
-                                                    <div class="form-group">
-                                                        <label for="">Site Phone</label>
-                                                        <input type="text" class="form-control" name="site_phone" value="{{ config('settings.site_phone') }}">
-                                                    </div>
-                                                </div>
-                                                <div class="col-md-12">
-                                                    <div class="form-group">
-                                                        <label for="">Site Default Currency</label>
-                                                        <Select name="site_default_currency" class="form-control select2">
-                                                            <option value="">Select</option>
-                                                            @foreach (config('currencies.currency_list') as $key => $currency)
-                                                                <option @selected($currency === config('settings.site_default_currency')) value="{{ $currency }}">{{ $key }} ({{ $currency }})</option>
-                                                            @endforeach
 
-                                                        </Select>
-                                                    </div>
-                                                </div>
+                                @include('admin.payment-setting.sections.paypal-settings')
 
-                                                <div class="col-md-6">
-                                                    <div class="form-group">
-                                                        <label for="">Site Currency Icon</label>
-                                                        <input type="text" class="form-control" name="site_currency_icon" value="{{ config('settings.site_currency_icon') }}">
-                                                    </div>
-                                                </div>
-
-                                                <div class="col-md-6">
-                                                    <div class="form-group">
-                                                        <label for="">Site Currency Possition</label>
-                                                        <Select name="site_currency_position" class="form-control">
-                                                            <option @selected(config('settings.site_currency_position') === 'left') value="left" >Left</option>
-                                                            <option @selected(config('settings.site_currency_position') === 'right') value="right">Right</option>
-                                                        </Select>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <button type="submit" class="btn btn-primary">Save</button>
-                                        </form>
-                                    </div>
-                                  </div>
-                                </div>
                                 <div class="tab-pane fade" id="profile4" role="tabpanel" aria-labelledby="profile-tab4">
                                   Sed sed metus vel lacus hendrerit tempus. Sed efficitur velit tortor, ac efficitur est lobortis quis. Nullam lacinia metus erat, sed fermentum justo rutrum ultrices. Proin quis iaculis tellus. Etiam ac vehicula eros, pharetra consectetur dui. Aliquam convallis neque eget tellus efficitur, eget maximus massa imperdiet. Morbi a mattis velit. Donec hendrerit venenatis justo, eget scelerisque tellus pharetra a.
                                 </div>
