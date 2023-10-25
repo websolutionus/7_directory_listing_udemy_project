ba4546164aa0f3de4ee79765a6ab50af06f153cb
Shakil Ahmed <sakilhossain01969@gmail.com>
2023-10-25 16:18:27 +0600

    Order Handling - Printing Invoice

--- resources/views/admin/order/show.blade.php
+++ resources/views/admin/order/show.blade.php
@@ -12,7 +12,7 @@
 
         <div class="section-body">
             <div class="invoice">
-                <div class="invoice-print">
+                <div class="invoice-print" id="invoice-print">
                     <div class="row">
                         <div class="col-lg-12">
                             <div class="invoice-title">
@@ -72,7 +72,7 @@
                                 </table>
                             </div>
                             <div class="row mt-4">
-                                <div class="col-lg-8">
+                                <div class="col-lg-8 d-print-none">
                                     <div class="section-title">Change Payment Status</div>
                                     <div class="col-md-4">
                                         <form action="{{ route('admin.orders.update', $order->id) }}" method="POST">
@@ -108,7 +108,7 @@
                     <div class="float-lg-left mb-lg-0 mb-3">
 
                     </div>
-                    <button class="btn btn-warning btn-icon icon-left"><i class="fas fa-print"></i> Print</button>
+                    <button class="btn btn-warning btn-icon icon-left" onclick="printPageArea('invoice-print')"><i class="fas fa-print"></i> Print</button>
                 </div>
             </div>
         </div>
@@ -116,5 +116,14 @@
 @endsection
 
 @push('scripts')
-    <script></script>
+    <script>
+        function printPageArea(areaId) {
+            var printContent = document.getElementById(areaId).innerHTML;
+            var originalContent = document.body.innerHTML;
+
+            document.body.innerHTML = printContent;
+            window.print();
+            document.body.innerHTML = originalContent;
+        }
+    </script>
 @endpush
