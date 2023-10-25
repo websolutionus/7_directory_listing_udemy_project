464e03f5111a1b318006da3ae037bef964a09271
Shakil Ahmed <sakilhossain01969@gmail.com>
2023-10-25 15:13:02 +0600

    Order Handling - Showing Order in View Page

--- resources/views/admin/order/show.blade.php
+++ resources/views/admin/order/show.blade.php
@@ -3,38 +3,111 @@
 @section('contents')
     <section class="section">
         <div class="section-header">
-            <div class="section-header-back">
-                <a href="{{ route('admin.orders.index') }}" class="btn btn-icon"><i class="fas fa-arrow-left"></i></a>
-            </div>
             <h1>Order Details</h1>
             <div class="section-header-breadcrumb">
                 <div class="breadcrumb-item active"><a href="{{ route('admin.dashboard.index') }}">Dashboard</a></div>
-                <div class="breadcrumb-item"><a href="{{ route('admin.orders.index') }}">order</a></div>
-                <div class="breadcrumb-item">show</div>
-
+                <div class="breadcrumb-item">Invoice</div>
             </div>
         </div>
 
         <div class="section-body">
-            <div class="row">
-                <div class="col-12">
-                    <div class="card">
-                        <div class="card-header">
-                            <h4>Order Details</h4>
-                        </div>
-                        <div class="card-body">
+            <div class="invoice">
+                <div class="invoice-print">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="invoice-title">
+                                <h2>Invoice</h2>
+                                <div class="invoice-number">Order #{{ $order->order_id }}</div>
+                            </div>
+                            <hr>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <address>
+                                        <strong>Billed To:</strong><br>
+                                        {{ $order->user->name }}<br>
+                                        {{ $order->user->email }}<br>
 
+                                    </address>
+                                </div>
+
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <address>
+                                        <strong>Payment Method:</strong><br>
+                                        {{ $order->payment_method }}<br>
+                                    </address>
+                                </div>
+                                <div class="col-md-6 text-md-right">
+                                    <address>
+                                        <strong>Order Date:</strong><br>
+                                        {{ date('F d, Y', strtotime($order->purchase_date)) }}<br><br>
+                                    </address>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row mt-4">
+                        <div class="col-md-12">
+                            <div class="section-title">Order Summary</div>
+                            <p class="section-lead">All items here cannot be deleted.</p>
+                            <div class="table-responsive">
+                                <table class="table table-striped table-hover table-md">
+                                    <tr>
+                                        <th data-width="40">#</th>
+                                        <th>Item</th>
+                                        <th class="text-center">Price</th>
+                                        <th class="text-center">Paid in </th>
+                                        <th class="text-right">Totals</th>
+                                    </tr>
+
+                                    <tr>
+                                        <td>1</td>
+                                        <td>{{ $order->package->name }}</td>
+                                        <td class="text-center">{{ $order->base_amount }} {{ $order->base_currency }}</td>
+                                        <td class="text-center">{{ $order->paid_amount }} {{ $order->paid_currency }}</td>
+                                        <td class="text-right">{{ $order->base_amount }} {{ $order->base_currency }}</td>
+                                    </tr>
+                                </table>
+                            </div>
+                            <div class="row mt-4">
+                                <div class="col-lg-8">
+                                    <div class="section-title">Payment Method</div>
+                                    <p class="section-lead">The payment method that we provide is to make it easier for you
+                                        to pay invoices.</p>
+                                    <div class="images">
+                                        <img src="assets/img/visa.png" alt="visa">
+                                        <img src="assets/img/jcb.png" alt="jcb">
+                                        <img src="assets/img/mastercard.png" alt="mastercard">
+                                        <img src="assets/img/paypal.png" alt="paypal">
+                                    </div>
+                                </div>
+                                <div class="col-lg-4 text-right">
+                                    <hr class="mt-2 mb-2">
+                                    <div class="invoice-detail-item">
+                                        <div class="invoice-detail-name">Total</div>
+                                        <div class="invoice-detail-value invoice-detail-value-lg">{{ $order->base_amount }} {{ $order->base_currency }}</div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
+                <hr>
+                <div class="text-md-right">
+                    <div class="float-lg-left mb-lg-0 mb-3">
+                        <button class="btn btn-primary btn-icon icon-left"><i class="fas fa-credit-card"></i> Process
+                            Payment</button>
+                        <button class="btn btn-danger btn-icon icon-left"><i class="fas fa-times"></i> Cancel</button>
+                    </div>
+                    <button class="btn btn-warning btn-icon icon-left"><i class="fas fa-print"></i> Print</button>
+                </div>
             </div>
-
         </div>
     </section>
 @endsection
 
 @push('scripts')
-    <script>
-
-    </script>
+    <script></script>
 @endpush
